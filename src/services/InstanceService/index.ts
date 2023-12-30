3c2d489e677e3967e779c0ef33f4be4d037aa893
Ben Allfree <ben@benallfree.com>
2023-12-30 17:05:18 +0000

    upgrade mode guard on nonzero spawn exit

--- src/services/InstanceService/index.ts
+++ src/services/InstanceService/index.ts
@@ -325,7 +325,7 @@ export const instanceService = mkSingleton(
         const pid = _pid()
         exitCode.then((code) => {
           info(`Processes exited with ${code}.`)
-          if (code !== 0) {
+          if (code !== 0 && !UPGRADE_MODE()) {
             shutdownManager.add(async () => {
               userInstanceLogger.error(
                 `Putting instance in maintenance mode because it shut down with return code ${code}. `,
