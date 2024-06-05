7c433fbda75e145f7ced33993a6e292259a69a64
Ben Allfree <ben@benallfree.com>
2024-06-05 15:44:25 -0700

    wip

--- packages/lander/_data/env.js
+++ packages/lander/_data/env.js
@@ -5,13 +5,14 @@
  */
 
 const PUBLIC_APEX_DOMAIN = process.env.PUBLIC_APEX_DOMAIN || 'pockethost.lvh.me'
+
 const env = {
   // The domain name where this dashboard lives
   PUBLIC_APP_URL:
     process.env.PUBLIC_APP_URL || `https://app.${PUBLIC_APEX_DOMAIN}`,
 
   PUBLIC_BLOG_URL:
-    process.env.PUBLIC_BLOG_URL || `https://${{ PUBLIC_APEX_DOMAIN }}`,
+    process.env.PUBLIC_BLOG_URL || `https://${PUBLIC_APEX_DOMAIN}`,
 
   PUBLIC_DISCORD_URL: `https://discord.gg/HsSjcuPRWX`,
 }
