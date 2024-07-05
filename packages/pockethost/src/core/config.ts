ebf25f4d23660676ca4c969c9a80614f0aa13def
Ben Allfree <ben@benallfree.com>
2024-07-05 07:00:52 -0700

    fix(pockethost): Fixed: 'config' now disallows invalid settings names

--- packages/pockethost/src/core/config.ts
+++ packages/pockethost/src/core/config.ts
@@ -1,7 +1,8 @@
 import { forEach, keys, omit, uniq } from '@s-libs/micro-dash'
-import { parse } from 'dotenv'
+import { DotenvParseOutput, parse } from 'dotenv'
 import { existsSync, readFileSync, writeFileSync } from 'fs'
 import { info } from '../cli'
+import { doSettingsFilter } from '../common'
 import { PH_HOME, settings } from '../constants'
 
 const envFile = () => {
@@ -10,6 +11,15 @@ const envFile = () => {
     writeFileSync(envFile, '')
   }
   return envFile
+}
+
+const configNameGuard = async (nameIn: string) => {
+  const _settings = await doSettingsFilter(settings)
+  const name = nameIn.toUpperCase()
+  if (!(name in _settings)) {
+    throw new Error(`Invalid config name ${nameIn}`)
+  }
+  return name
 }
 
 const _parse = () =>
@@ -25,8 +35,9 @@ function write(values: DotenvParseOutput) {
   info(`Written to ${envFile()}`)
 }
 
-export const setConfig = async (name: string, value: string) => {
+export const setConfig = async (nameIn: string, value: string) => {
   if (value === '=') throw new Error(`Invalid value ${value}`)
+  const name = await configNameGuard(nameIn)
 
   const values = _parse()
   values[name] = value
@@ -43,7 +54,7 @@ export const unsetConfig = (name: string) => {
   write(values)
 }
 
-export const listConfig = () => {
+export const listConfig = async () => {
   const values = _parse()
 
   if (keys(values).length > 0) {
@@ -70,7 +81,8 @@ export const listConfig = () => {
   }
 }
 
-export const appendConfig = (name: string, value: string) => {
+export const appendConfig = async (nameIn: string, value: string) => {
+  const name = await configNameGuard(nameIn)
   const values = _parse()
   values[name] = uniq([
     ...(values[name]
@@ -83,7 +95,8 @@ export const appendConfig = (name: string, value: string) => {
   write(values)
 }
 
-export const filterConfig = (name: string, value: string) => {
+export const filterConfig = async (nameIn: string, value: string) => {
+  const name = await configNameGuard(nameIn)
   const values = _parse()
   values[name] = uniq([
     ...(values[name]
